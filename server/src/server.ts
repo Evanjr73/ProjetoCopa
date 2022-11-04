503074818b2c9be50ee94089c4352ac6b8dac5f5
Evan junior <evsioljr@gmail.com>
2022-11-03 21:39:14 -0300

    testando portas

--- server/src/server.ts
+++ server/src/server.ts
@@ -22,6 +22,6 @@ async function bootstrap() {
        const count = await prisma.pool.count()
         return {count }
     })
-    await fastify.listen({ port:3333 , host:'0.0.0.0'})
+    await fastify.listen({ port:3333 ,})
 }
 bootstrap() 
